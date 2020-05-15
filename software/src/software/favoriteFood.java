18253350b3bdd52bb39d279a47d97a81d38d4736
suzukirisa <suzukirisa@192.168.2.104>
2020-05-15 17:58:55 +0900

    かきくけこ

--- software/src/software/favoriteFood.java
+++ software/src/software/favoriteFood.java
@@ -4,8 +4,8 @@ public class favoriteFood {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("好きな食べ物はアイスクリームです。");
-		System.out.println("理由は甘いからです。");
+		System.out.println("好きな食べ物はアイスクリームです！");
+		System.out.println("理由は甘いからです！");
 	}
 
 }
